1fb551f2ab4d40f64a048189c4c49ad930711477
sudhamshk <sudhamsh543@gmail.com>
2017-08-16 15:04:02 +0530

    Sudhamsh, David | #3607 | Added ability to parse drugs

--- mtc_import/mtc_form.py
+++ mtc_import/mtc_form.py
@@ -1,3 +1,17 @@
+class DotRatePerDrugForm:
+    def __init__(self, drug_name, csv_row):
+        self.drug_name = drug_name
+        self.prescribed_days = self.parse_days(csv_row, 'D' + drug_name)
+        self.observed_days = self.parse_days(csv_row, 'O' + drug_name)
+        self.missed_days = self.parse_days(csv_row, 'M' + drug_name)
+
+    def parse_days(self, csv_row, column):
+        string_value = csv_row.get(column)
+        return int(string_value) if string_value else None
+
+    def is_valid(self):
+        return self.prescribed_days and self.observed_days and self.missed_days
+
 class MTCForm:
     def __init__(self, csv_row):
         self.registration_number = csv_row['RegistrationNb']
@@ -7,3 +21,15 @@ class MTCForm:
         self.non_prescribed_days = self.ideal_treatment_days - int(csv_row['PrecribedDays'])
         self.missed_prescribed_days = int(csv_row['MissedDays'])
         self.incomplete_prescribed_days = int(csv_row['UncomplDays'])
+        self.dot_rate_per_drug = []
+
+        self.parse_dot_rate_per_drug(csv_row)
+
+    def parse_dot_rate_per_drug(self, csv_row):
+        for column_name, value in csv_row.iteritems():
+            if column_name[0] == 'D':
+                drug_name = column_name[1:10]
+                drug_form = DotRatePerDrugForm(drug_name, csv_row)
+
+                if drug_form.is_valid():
+                    self.dot_rate_per_drug.append(drug_form)
--- tests/mtc_form_test.py
+++ tests/mtc_form_test.py
@@ -1,12 +1,12 @@
 from pytest import fixture
-
 from mtc_import.mtc_form import MTCForm
+
 
 def describe_mtc_form():
 
     @fixture
-    def mock_csv_row():
-        return {
+    def mock_csv_row(data={}):
+        data.update({
             'RegistrationNb': 'someRegistrationNumber',
             'Obs_Month': '12',
             'Obs_year' : '2015',
@@ -14,7 +14,8 @@ def describe_mtc_form():
             'PrecribedDays':'2',
             'MissedDays': '3',
             'UncomplDays': '4'
-        }
+        })
+        return data
 
 
     def should_assign_properties_from_csv_row():
@@ -32,3 +33,48 @@ def describe_mtc_form():
         form = MTCForm(csv_row)
 
         assert form.non_prescribed_days == 21
+
+    def describe_dot_rate_per_drug():
+        def should_include_drugs_with_all_three_values_given():
+            csv_row = mock_csv_row({
+                'DBdq': '15',
+                'OBdq': '8',
+                'MBdq': '5'
+            })
+            form = MTCForm(csv_row)
+            assert len(form.dot_rate_per_drug) == 1
+            drug_form = form.dot_rate_per_drug[0]
+            assert drug_form.drug_name == 'Bdq'
+            assert drug_form.prescribed_days == 15
+            assert drug_form.observed_days == 8
+            assert drug_form.missed_days == 5
+
+        def should_not_include_drugs_with_missing_values():
+            csv_row = mock_csv_row({
+                'DBdq': '15',
+                'OBdq': '',
+                'MBdq': ''
+            })
+            form = MTCForm(csv_row)
+            assert len(form.dot_rate_per_drug) == 0
+
+        def should_not_include_drugs_without_any_values():
+            csv_row = mock_csv_row({
+                'DBdq': '',
+                'OBdq': '',
+                'MBdq': ''
+            })
+            form = MTCForm(csv_row)
+            assert len(form.dot_rate_per_drug) == 0
+
+        def should_include_multiple_drugs():
+            csv_row = mock_csv_row({
+                'DBdq': '15',
+                'OBdq': '8',
+                'MBdq': '6',
+                'DDlm': '12',
+                'ODlm': '4',
+                'MDlm': '3'
+            })
+            form = MTCForm(csv_row)
+            assert len(form.dot_rate_per_drug) == 2
